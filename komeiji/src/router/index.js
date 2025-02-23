8ca4955e916c7a7204e16269a63d9e3176139d5a
KirisameVanilla <118162831+KirisameVanilla@users.noreply.github.com>
2025-02-23 12:44:43 +0800

    wip: add empty homepage (#11)
    * style: rename HomeView to LoginView
    
    * wip: HomeView
    
    * wip: cookies storage structure Vuex
    
    * revert: "wip: cookies storage structure Vuex"
    
    This reverts commit 10d9b7febb76b6867be93e2870308641b125d01d.

--- komeiji/src/router/index.js
+++ komeiji/src/router/index.js
@@ -1,13 +1,19 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
 import AboutView from "@/views/AboutView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: HomeView,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: LoginView,
     },
     {
         path: "/about",
--- komeiji/src/views/HomeView.vue
+++ komeiji/src/views/HomeView.vue
@@ -1,80 +1,13 @@
+<script setup>
+// 这里可以添加逻辑代码
+</script>
+
 <template>
-  <div class="login-container">
-    <h2>登录</h2>
-
-    <InputBlank ref="usernameRef" :x="50" :y="100" placeholder="用户名" />
-
-    <InputBlank ref="passwordRef" :x="50" :y="150" placeholder="密码" type="password" />
-
-    <button class="login-button" @click="handleLogin">登录</button>
-
-    <label>
-      <input type="checkbox" v-model="isRegistering" />
-      {{ "注册" }}
-    </label>
+  <div>
+    <h1>About</h1>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import InputBlank from "@/components/InputBlank.vue";
-import userApi from "@/api/userApi.js";
-import router from "@/router/index.js";
-
-const usernameRef = ref(null);
-const passwordRef = ref(null);
-const isRegistering = ref(false);
-
-const handleLogin = () => {
-  const username = usernameRef.valueOf().value.getValue();
-  const password = passwordRef.valueOf().value.getValue();
-
-  if (!username || !password) {
-    alert("请输入用户名和密码！");
-    return;
-  }
-
-  if (isRegistering) {
-    userApi.register(username, password).then((res) => {
-      alert(res);
-    })
-  } else {
-    userApi.login(username, password).then((res) => {
-      alert(res);
-    })
-  }
-
-  alert(`登录成功！用户名：${username}`);
-  router.push("about")
-};
-</script>
-
 <style scoped>
-.login-container {
-  width: 300px;
-  padding: 20px;
-  background: #fff;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  position: relative;
-}
-
-h2 {
-  text-align: center;
-}
-
-.login-button {
-  width: 100%;
-  margin-top: 160px;
-  padding: 10px;
-  background: #007bff;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.login-button:hover {
-  background: #0056b3;
-}
+/* 这里可以添加样式 */
 </style>
