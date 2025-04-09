60a4eb1fafa10dd7e865ebbb9213fdcd5dbb69e7
KirisameVanilla <vanillaaaajp@gmail.com>
2025-04-09 10:11:58 +0800

    feat: Multi Requests

--- src/views/AssistantWorkbench.vue
+++ src/views/AssistantWorkbench.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="assistant-workbench">
     <h2>咨询师工作台</h2>
-    <div v-if="currentRequest" class="request-panel">
-      <h3>新的咨询请求</h3>
-      <p>用户ID: {{ currentRequest.userId }}</p>
-      <p>消息: {{currentRequest.message}}</p>
-      <div class="button-group">
-        <button @click="handleRequest(true)" class="accept-btn">接受</button>
-        <button @click="handleRequest(false)" class="reject-btn">拒绝</button>
+    <div v-if="requests.length" class="request-list">
+      <div v-for="(request, index) in requests" :key="index" class="request-panel">
+        <h3>新的咨询请求</h3>
+        <p>用户ID: {{ request.userId }}</p>
+        <p>消息: {{ request.message }}</p>
+        <div class="button-group">
+          <button @click="handleRequest(true, index)" class="accept-btn">接受</button>
+          <button @click="handleRequest(false, index)" class="reject-btn">拒绝</button>
+        </div>
       </div>
     </div>
+    <div v-else class="no-request">
+      <p>等待新的咨询请求...</p>
+    </div>
+
     <div v-else class="no-request">
       <p>等待新的咨询请求...</p>
     </div>
@@ -25,7 +31,7 @@ export default {
   data() {
     return {
       ws: null,
-      currentRequest: null
+      requests: []
     }
   },
   created() {
@@ -49,10 +55,10 @@ export default {
         const data = JSON.parse(event.data)
         if (data.type === 'chat_request') {
           const requestJson = JSON.parse(data.content)
-          this.currentRequest = {
+          this.requests.push({
             userId: requestJson.patientId,
             message: requestJson.message,
-          }
+          });
         } else if (data.type === 'chat_connect') {
           const newSocketAddress = "ws://127.0.0.1:54950/ws?from="+JSON.parse(data.content).from+"&to="+JSON.parse(data.content).to;
           localStorage.setItem('chatAddress', newSocketAddress)
