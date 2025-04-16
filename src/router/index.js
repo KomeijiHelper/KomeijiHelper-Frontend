771c927fc3200d9b272121f81d630a4943c956cd
Tofweod <junqiaochen@qq.com>
2025-04-16 10:24:51 +0800

    wip

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import {ClearLocalStorage} from "@/utils.js";
 import RegisterView from "@/views/RegisterView.vue";
 import ProfileView from "@/views/ProfileView.vue";
 import FAQView from "@/views/FAQView.vue";
+import ChatRecordTestView from '@/views/ChatRecordTestView.vue'
 
 const routes = [
     {
@@ -20,6 +21,12 @@ const routes = [
         name: "Home",
         component: HomeView,
         meta: { needAuth: false, roles: [-1, 0, 1, 2, 3]}
+    },
+    {
+        path:"/chatrecord",
+        name:"ChatRecord",
+        component: ChatRecordTestView,
+        meta:{ needAuth:false, roles:[-1,0,1,2,3]}
     },
     {
         path: "/login",
