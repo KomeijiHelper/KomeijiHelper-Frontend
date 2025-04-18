75b6eebe80b075451b4e35a4303a2395adef8086
KirisameVanilla <vanillaaaajp@gmail.com>
2025-04-19 02:06:37 +0800

    production

--- src/components/SelectConsultant.vue
+++ src/components/SelectConsultant.vue
@@ -81,7 +81,7 @@ export default {
     },
     setupWebSocket() {
       const id = localStorage.getItem('userName');
-      this.ws = new WebSocket('ws://127.0.0.1:54950/ws?id='+id)
+      this.ws = new WebSocket('wss://komeiji.cyou:54950/ws?id='+id)
       
       this.ws.onopen = () => {
         console.log('WebSocket连接已建立')
@@ -91,7 +91,7 @@ export default {
         const data = JSON.parse(event.data)
         console.log(data)
         if (data.type === 'chat_connect') {
-          const newSocketAddress = "ws://127.0.0.1:54950/ws?from="+JSON.parse(data.content).from+"&to="+JSON.parse(data.content).to;
+          const newSocketAddress = "wss://komeiji.cyou:54950/ws?from="+JSON.parse(data.content).from+"&to="+JSON.parse(data.content).to;
           localStorage.setItem('chatAddress', newSocketAddress)
           router.push("/chat")
         }
